ba21880d29911479cfa99ebc0559a59ed449a776
brownb202 <brownb202@gmail.com>
2020-10-31 12:08:01 -0600

    app now displays a random image from the first page of the object results

--- server/server.js
+++ server/server.js
@@ -4,14 +4,18 @@ const app = express();
 const PORT = 8080;
 
 const path = require("path");
-const Key = require('./hamApiKey.js')
+const Key = require('./actualAPIKey.js')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, '../public')));
-console.log('here is the key: ', Key)
-app.get(`https://api.harvardartmuseums.org/object?apikey=${Key}`)
+
+// app.get(`https://api.harvardartmuseums.org/object?apikey=${Key}`, (req, res) => {
+//   res.send(res)
+// })
+
+
+
 app.listen(PORT, () => {
   console.log(`Hello, Scrumdog. Your server is running on PORT: ${PORT}`);
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
+import Axios from 'axios';
+import Key from '../server/actualAPIKey.js';
 
-class App extends Component{
-  render(){
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [randomPicture, setRandomPicture] = useState([])
+
+  let randomInt = Math.floor(Math.random() * 5)
+  console.log(randomInt)
+useEffect(() => {
+    Axios.get(`https://api.harvardartmuseums.org/object?apikey=${Key}`)
+  .then(data => setRandomPicture(data.data.records[randomInt].images[0].baseimageurl))
+});
+
+
     return(
+
       <div className="App">
         <h1> Hello, World! </h1>
+        <p>
+          {`You clicked ${count} times.`}
+        </p>
+        <button className="ri-button" onClick={() => setCount(count + 1)} type="submit">Dangerous Button</button>
+    <div>Random Pic:
+      <img src={randomPicture}></img>
+    </div>
       </div>
     );
-  }
+
+
+
+
+
 }
 
 export default App;
