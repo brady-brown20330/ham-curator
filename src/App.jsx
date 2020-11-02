964462ccb1ee8a3073b75788bd2b716c48865995
brownb202 <brownb202@gmail.com>
2020-11-02 16:18:31 -0700

    added titlebar and styled buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,43 @@ import Axios from 'axios';
 import Key from '../server/actualAPIKey.js';
 import PhotoList from './components/photoList.jsx';
 
+
+//material-ui imports
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+
+  appBar: {
+    backgroundColor: "black",
+  },
+  classyButton: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    '& > *': {
+      margin: theme.spacing(1),
+     }
+  },
+}));
+
 const App = () => {
+  const classes = useStyles();
   const [count, setCount] = useState(0);
   const [randomPicture, setRandomPicture] = useState([])
 
@@ -34,10 +70,26 @@ const prevPage = () => {
  }
 
     return(
-      <div className="App">
-        <h1> Page through our collection: </h1>
-        <button onClick={prevPage}>Previous</button>
-        <button onClick={nextPage}>Next</button>
+      <div>
+        <div className={classes.root}>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+          Page through our collection:
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+        <ButtonGroup variant="text" color="primary" aria-label="text primary button group" className={classes.classyButton}>
+          <Button onClick={prevPage} color="black">Previous</Button>
+            <div></div>
+          <Button onClick={nextPage} color="black">Next</Button>
+        </ButtonGroup>
+
         <PhotoList list={randomPicture} />
       </div>
     );
